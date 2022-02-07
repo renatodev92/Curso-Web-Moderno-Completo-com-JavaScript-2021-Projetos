926ac366479d1946ef783291ad502f814661dabc
renatodev92 <renato.roliveira92@gmail.com>
2022-02-07 10:11:39 -0300

    Adicionando a pag Stas

--- node/nodeRockseat/stats.js
+++ node/nodeRockseat/stats.js
@@ -1,3 +1,28 @@
 const os = require('os')
 
-console.log(os.version());
+
+
+
+
+setInterval(() => {
+    const { freemem, totalmem } = os
+
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((mem / total) * 100)
+
+
+
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+
+    }
+
+    console.clear()
+    console.log(`=======PC STATS=======`)
+    console.table(stats)
+
+
+}, 5000)
